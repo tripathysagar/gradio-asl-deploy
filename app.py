370f576c50796bf7693141f3c2e1218bf4fee081
tripathysagar <tripathysagar08@gmail.com>
2024-02-19 08:11:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from fastai.vision.all import *
+
+
+# In[ ]:
+
+
+from pathlib import Path
+from fastcore.xtras import Path
+from fastai.learner import load_learner
+from fastcore.foundation import L
+
+
+# In[ ]:
+
+
+img_path = Path('./static')
+images_lis = [ i for i in list(img_path.ls()) if i.is_file()]
+images_lis[:3], len(images_lis)
+
+
+# In[ ]:
+
+
+def label_to_class(f:str)->L:
+    return L(f)
+
+
+# In[ ]:
+
+
+model_path = Path('./model/model_asl_sign01.pkl')
+learner = load_learner('./model/model_asl_sign01.pkl')
+
+
+# In[ ]:
+
+
+import gradio as gr
+import os
+
+
+def pred_image(image):
+    dict = {}
+    for i, j in zip(learner.dls.vocab, learner.predict(images_lis[0])[2]):
+        dict[i] = round(j.item(), 2)
+    
+    return dict
+
+
+demo = gr.Interface(
+    fn=pred_image,
+    inputs='image',
+    outputs=gr.Label(num_top_classes=29),
+    examples=images_lis,
+)
+
+if __name__ == "__main__":
+    demo.launch(debug=True)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
